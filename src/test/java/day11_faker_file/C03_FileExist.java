eeac84a2a5656ab68de2bfedd5bdb088bc9d6833
DzKKaya <kayalik007@gmail.com>
2022-05-20 23:33:10 +0300

    20.05.2022-2

---
+++ src/test/java/day11_faker_file/C03_FileExist.java
@@ -0,0 +1,27 @@
+package day11_faker_file;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import java.nio.file.Files;
+import java.nio.file.Paths;
+
+public class C03_FileExist {
+    @Test
+    public void test01() {
+        System.out.println(System.getProperty("user.dir"));
+        //C:\Users\LG\IdeaProjects\com.Batch59Selenium
+
+        System.out.println(System.getProperty("user.home"));
+        //C:\Users\LG\Downloads
+
+        String dosyaYolu=System.getProperty("user.home")+"\\Desktop\\text.txt";
+
+        System.out.println(dosyaYolu);
+
+        System.out.println(Files.exists(Paths.get("C:\\Users\\LG\\Desktop\\text.txt")));
+
+        Assert.assertTrue(Files.exists(Paths.get(dosyaYolu)));
+
+    }
+}
