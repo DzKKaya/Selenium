c548f04bf379dc79c22bc0b31df441ef0b3a113c
DzKKaya <kayalik007@gmail.com>
2022-05-27 00:12:33 +0300

    26.05.2022

--- src/test/java/practice3/Q07_DropDown.java
+++ src/test/java/practice3/Q07_DropDown.java
@@ -1,11 +1,38 @@
 package practice;
 
-public class Q07_DropDown {
-    // go to url : https://www.jqueryscript.net/demo/Drop-Down-Combo-Tree/
-    //-->Task1 Find Dropdown on Multi Selection
-    //-->Task2 Find  all Dropdown Elements on page
-    //-->Task3 printout DropDown Elements' number
-    //-->Task4 choose dropdown elements and printout dropdown elements' name  until choise 6
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.Select;
+import utilities.TestBase;
+
+import java.util.List;
+
+public class Q07_DropDown extends TestBase {
+    @Test
+    public void dropDown() {
+        // go to url : https://www.jqueryscript.net/demo/Drop-Down-Combo-Tree/
+        driver.get("https://www.jqueryscript.net/demo/Drop-Down-Combo-Tree/");
+
+        //-->Task1 Find Dropdown on Multi Selection
+        WebElement mSelection=driver.findElement(By.xpath("//*[@id='justAnInputBox']"));
+        Select select = new Select(mSelection);
+        mSelection.click();
+        System.out.println(mSelection.getText());
+        //-->Task2 Find  all Dropdown Elements on page
+        List<WebElement> allDDE=driver.findElements(By.xpath("//*[@class='comboTreeItemTitle']"));
+        allDDE.stream().map(t->t.getText()).forEach(System.out::println);
+
+        List<WebElement> dropDown=driver.findElements(By.xpath("//*[@class='ComboTreeItemParent']"));
+        dropDown.stream().forEach(t->System.out.println(t.getText()+ " "));
+        //-->Task3 printout DropDown Elements' number
+        System.out.println("DropDown Element'leri sayisi :"+dropDown.size());
+        //-->Task4 choose dropdown elements and printout dropdown elements' name  until choise 6
+
+
+    }
+
+
 
 
 
--- src/test/java/practice3/Q10_Iframe.java
+++ src/test/java/practice3/Q10_Iframe.java
@@ -1,13 +1,34 @@
 package practice;
 
-public class Q10_Iframe  {
-      /*
-    // https://www.jqueryscript.net/demo/Easy-iFrame-based-Twitter-Emoji-Picker-Plugin-jQuery-Emoojis/ sitesine gidin
-    // web sitesini maximize yapin
-    // ikinci emojiye tıklayın
-    // tüm ikinci emoji öğelerini tıklayın
-    // parent iframe e geri donun
-    // formu doldurun,(Formu istediğiniz metinlerle doldurun)
-    //  apply button a basin
- */
+import org.junit.Test;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.interactions.Actions;
+import utilities.TestBase;
+
+import java.util.List;
+
+public class Q10_Iframe  extends TestBase {
+    @Test
+    public void iframeClass() {
+        // https://www.jqueryscript.net/demo/Easy-iFrame-based-Twitter-Emoji-Picker-Plugin-jQuery-Emoojis/ sitesine gidin
+       driver.get("https://www.jqueryscript.net/demo/Easy-iFrame-based-Twitter-Emoji-Picker-Plugin-jQuery-Emoojis/");
+
+        WebElement iframe=driver.findElement(By.xpath("//iframe[@id='emoojis']"));
+        driver.switchTo().frame(iframe);
+        Actions actions=new Actions(driver);
+        WebElement ayi=driver.findElement(By.xpath("(//span[@class='mdl-tabs__ripple-container mdl-js-ripple-effect'])[2]"));
+
+        // ikinci emojiye tıklayın
+        actions.moveToElement(ayi).click().perform();
+        // tüm ikinci emoji öğelerini tıklayın
+        List<WebElement> tumEmoji=driver.findElements(By.xpath("//div[@class='mdl-tabs__panel is-active']//div//img"));
+        tumEmoji.stream().forEach(t->t.click());
+
+        // parent iframe e geri donun
+        // formu doldurun,(Formu istediğiniz metinlerle doldurun)
+        //  apply button a basin
+
+    }
+
 }
